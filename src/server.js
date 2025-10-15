fd1ef223a48a9cab300b3a01d1ad4c9d6b08766d
blaison160 <emilie.starsquid@gmail.com>
2025-10-15 10:01:42 +0200

    [REFACTOR] server is now an express app

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 import http from 'http'
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer((req,res) => {
-    res.end("Hello World !")
+app.router.get('/questions',(req,res) =>{
+    res.status(200).send([
+        {
+            id : "0",
+            question: "Quelle est la capitale de la France ?",
+            answer: "Paris"
+        },
+    ])
 })
 
-server.listen(PORT, () => {
+app.post('/questions',(req,res) =>{
+    const {question, answear} = req.body
+
+    if (!question || !answear){
+        return res(400).send({error: 'Question or answear undefined'})
+    }
+
+    return res(201).send({message: 'Question created'})
+
+})
+
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
+
